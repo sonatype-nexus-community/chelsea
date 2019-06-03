11402b97dc366baa8b452053c2155464a3738965
Allister Beharry <allister.beharry@gmail.com>
2019-06-03 14:22:29 -0400

    Add get_dependencies method.

--- auditrb.gemspec
+++ auditrb.gemspec
@@ -56,6 +56,8 @@ Gem::Specification.new do |spec|
   spec.add_dependency "tty-which", "~> 0.4"
   spec.add_dependency "pastel", "~> 0.7.2"
   spec.add_dependency "thor", "~> 0.20.0"
+  spec.add_dependency "semantic" "~> 1.6.1"
+  spec.add_dependency "foo", "> 2.2.0"
 
   spec.add_development_dependency "bundler", "~> 1.17"
   spec.add_development_dependency "rake", "~> 10.0"
--- lib/auditrb/commands/gems.rb
+++ lib/auditrb/commands/gems.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'pastel'
+require 'semantic'
 require_relative '../command'
 
 module Auditrb
@@ -10,20 +11,20 @@ module Auditrb
         @file = file
         @options = options
         @pastel = Pastel.new
-
+        @dependencies = Hash.new()
+        @versions
       end
 
       def execute(input: $stdin, output: $stdout)
-        show_logo()
         if not gemspec_file_exists?
           return
         end
-      end
-
-      def show_logo()
-        require 'tty-font'
-        font = TTY::Font.new(:doom)
-        $stdout.puts @pastel.green(font.write("auditrb"))
+        n = get_dependencies()
+        puts "Parsed #{n} dependencies."
+        if n == 0
+          print_err "No dependencies retrieved. Exiting."
+          return
+        end
       end
 
       def gemspec_file_exists?()
@@ -33,20 +34,46 @@ module Auditrb
         else
           require 'pathname'
           path = Pathname.new(@file)  
-          print_ok "Using .gemspec file #{path.realpath}."
+          print_success "Using .gemspec file #{path.realpath}."
           return true
         end
       end
 
+      def get_dependencies()
+        IO.foreach(@file) do |x|
+          case x
+          when /^\s*spec\.add_dependency\s+"?([^"]+)"?,\s*(.+)$/
+            p = $1
+            v = $2.to_s
+            r = 
+              if v.start_with?('"') then 
+                v.gsub!(/\A"|"\Z/, '') 
+              else 
+                v 
+              end 
+            @dependencies[p] = Gem::Requirement.parse(r)
+          end
+        end
+        @dependencies.count()
+      end
+
+      def get_dependencies_versions()
+        @dependencies.each do |p, v|
+          v
+          #spec = v.to_spec()
+          #version = Gem::Version.new(v.)
+          rescue Gem::MissingSpecError => error
+            print_err "Parsing dependency #{p} failed: #{error}."       
+        end
+      end
+
       def print_err(s)
-        $stdout.puts @pastel.red.bold(s)
+        puts @pastel.red.bold(s)
       end
 
-      def print_ok(s)
-        $stdout.puts @pastel.green.bold(s)
+      def print_success(s)
+        puts @pastel.green.bold(s)
       end
-
-
 
     end
   end
