011d4d38c32b5d6192a11f9c8453e4ce88a1982d
Jeffry Hesse <jhesse@sonatype.com>
2020-03-29 10:34:15 -0800

    WIP

--- spec/unit/gems_spec.rb
+++ spec/unit/gems_spec.rb
@@ -1,4 +1,12 @@
 require 'chelsea/gems'
+require_relative '../spec_helper'
+
+RSpec.configure do |config|
+  config.before(:each) do
+    stub_request(:post, 'https://ossindex.sonatype.org/api/v3/component-report').
+      to_return(status: 200, body: "", headers: {})
+  end
+end
 
 RSpec.describe Chelsea::Gems do
   it "executes `gems` command successfully" do
@@ -10,5 +18,13 @@ RSpec.describe Chelsea::Gems do
     command.execute(output: output)
 
     expect(output.string).to eq("")
+  end
+
+  it "can handle requests to OSS Index" do
+      file = "Gemfile.lock"
+      options = {}
+      command = Chelsea::Gems.new(file, options)
+
+      command.get_vulns()
   end
 end
