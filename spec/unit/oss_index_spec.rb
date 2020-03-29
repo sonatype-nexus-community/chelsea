3ac830b68f7eefcd041c032753882d466d3340eb
Jeffry Hesse <jhesse@sonatype.com>
2020-03-29 15:58:20 -0800

    Try this

--- spec/unit/oss_index_spec.rb
+++ spec/unit/oss_index_spec.rb
@@ -3,19 +3,23 @@ require_relative '../spec_helper'
 require 'chelsea/oss_index_exception'
 
 RSpec.describe Chelsea::OssIndex do
-  it "returns an array of results from OSS Index when queried properly" do
-    stub_request(:post, 'https://ossindex.sonatype.org/api/v3/component-report').
+  describe "when talking to OSS Index" do
+    before(:all) {
+      stub_request(:post, 'https://ossindex.sonatype.org/api/v3/component-report').
       to_return(status: 200, body: OSS_INDEX_RESPONSE, headers: {})
+    }
+    
+    it "returns an array of results from OSS Index when queried properly" do
+      file = "Gemfile.lock"
+      options = Hash.new
+      options[:db_file_name] = 'chelsea-test.pstore'
+      ossindex = Chelsea::OssIndex.new(options)
 
-    file = "Gemfile.lock"
-    options = Hash.new
-    options[:db_file_name] = 'chelsea-test.pstore'
-    ossindex = Chelsea::OssIndex.new(options)
+      ossindex.clear_db_cache()
 
-    ossindex.clear_db_cache()
-
-    server_response = ossindex.query_ossindex_for_vulns(get_coordinates())
-    expect(server_response.count()).to eq(25)
+      server_response = ossindex.query_ossindex_for_vulns(get_coordinates())
+      expect(server_response.count()).to eq(25)
+    end
   end
 
   it "handles a 404 response from the remote adequately" do
