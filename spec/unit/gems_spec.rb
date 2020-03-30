0f55c0eefb03dc82ae6b97c8bda176751591ab46
Jeffry Hesse <jhesse@sonatype.com>
2020-03-29 16:00:27 -0800

    Mock this too

--- spec/unit/gems_spec.rb
+++ spec/unit/gems_spec.rb
@@ -1,15 +1,23 @@
 require 'chelsea/gems'
+require_relative '../spec_helper'
 
 RSpec.describe Chelsea::Gems do
-  it "can collect dependencies, query, and print results" do
-    output = StringIO.new
-    file = "spec/testdata/Gemfile.lock"
-    options = {}
-    command = Chelsea::Gems.new(file, options)
+  describe "when talking to OSS Index" do
+    before(:all) {
+      stub_request(:post, 'https://ossindex.sonatype.org/api/v3/component-report').
+      to_return(status: 200, body: OSS_INDEX_RESPONSE, headers: {})
+    }
 
-    command.execute(output: output)
+    it "can collect dependencies, query, and print results" do
+      output = StringIO.new
+      file = "spec/testdata/Gemfile.lock"
+      options = {}
+      command = Chelsea::Gems.new(file, options)
 
-    expect(output.string).to eq("")
+      command.execute(output: output)
+
+      expect(output.string).to eq("")
+    end
   end
 
   it "will exit if a invalid Gemfile.lock is passed" do
