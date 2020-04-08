dfe13e36976572814a4090da0f1b1fb314d8ddc8
Glenn Mohre <glenn.mohre@gmail.com>
2020-04-08 14:00:03 -0400

    added bom spec

---
+++ spec/unit/bom_spec.rb
@@ -0,0 +1,15 @@
+require 'chelsea/bom'
+require 'chelsea/deps'
+require 'ox'
+require_relative '../spec_helper'
+RSpec.describe Chelsea::Bom do
+  before do
+     @deps = get_test_dependencies
+  end
+
+  it "can render dependencies as xml" do
+    bom = Chelsea::Bom.new(@deps)
+    expect(bom.xml.class).to eq(Ox::Document)
+    expect(bom.to_s.class).to eq(String)
+  end
+end
