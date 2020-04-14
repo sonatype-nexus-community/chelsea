922a0db9fa7e1328b81113e8727c617cd71240ee
Glenn Mohre <glenn.mohre@gmail.com>
2020-04-14 13:11:37 -0400

    more cleaning

--- lib/chelsea/cli.rb
+++ lib/chelsea/cli.rb
@@ -11,7 +11,6 @@ module Chelsea
   ##
   # This class provides an interface to the oss index, gems and deps
   class CLI
-
     def initialize(opts)
       @opts = opts
       @pastel = Pastel.new
@@ -22,41 +21,49 @@ module Chelsea
     def process!
       if @opts.config?
         _set_config # move to init
+        exit
       end
       if @opts.file? # don't process unless there was a file
-        @gems = Chelsea::Gems.new(file: @opts[:file], quiet: @opts[:quiet], options: @opts)
-        @gems.execute # should be more like collect
-        if @opts.sbom?
-          @iq = Chelsea::IQClient.new(@opts[:application], @opts[:server], @opts[:iquser], @opts[:iqpass])
-          bom = Chelsea::Bom.new(@gems.deps)
-          @iq.submit_sbom(bom)
-        end
+        _process_file
       elsif @opts.help? # quit on opts.help earlier
         puts _cli_flags # this doesn't exist
       end
     end
 
-    # this is how you do static methods in ruby, because in a test we want to 
-
-    # check for version without opts, and heck, we don't even want a dang object!
     def self.version
       Chelsea::VERSION
     end
 
-  protected
+    private
+
+    def _process_file
+      gems = Chelsea::Gems.new(
+        file: @opts[:file],
+        quiet: @opts[:quiet],
+        options: @opts
+      )
+      gems.execute # should be more like collect
+      return unless @opts.sbom?
+
+      @iq = Chelsea::IQClient.new(
+        @opts[:application],
+        @opts[:server],
+        @opts[:iquser],
+        @opts[:iqpass]
+      )
+      bom = Chelsea::Bom.new(@gems.deps)
+      @iq.submit_sbom(bom)
+    end
 
     def _flags_error
-      # should be custom exception! 
-      switches = _flags.collect {|f| "--#{f}"}
-
+      switches = _flags.collect { |f| "--#{f}" }
       abort "please set one of #{switches}"
     end
 
     def _validate_arguments
-      if !_flags_set? && !@opts.file?
-        ## require at least one argument
-        _flags_error
-      end
+      return unless !_flags_set? && !@opts.file?
+
+      _flags_error
     end
 
     def _flags_set?
@@ -78,7 +85,7 @@ module Chelsea
 
     def _load_config
       config = Chelsea::Config.new
-      config.get_oss_index_config()
+      config.get_oss_index_config
     end
 
     def _set_config
