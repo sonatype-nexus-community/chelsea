35918d79992d02baebf4920ddd238818bd988be9
Jeffry Hesse <jhesse@sonatype.com>
2020-04-21 09:39:25 -0800

    tidy

--- lib/chelsea/formatters/text.rb
+++ lib/chelsea/formatters/text.rb
@@ -30,7 +30,6 @@ module Chelsea
           response += @pastel.red("[#{i}/#{count}] - #{package} ") + @pastel.red.bold("Vulnerable.\n")
           response += _get_reverse_deps(reverse_deps, name) if reverse_deps
           r['vulnerabilities'].each do |k, _|
-            puts k
             response += _format_vuln(k)
           end
         else
