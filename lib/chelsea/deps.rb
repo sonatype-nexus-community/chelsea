e1a81380c006b83b0988c672b69149c88b33c1dc
Glenn Mohre <glenn.mohre@gmail.com>
2020-04-24 16:20:37 -0400

    ripping this pile down

--- lib/chelsea/deps.rb
+++ lib/chelsea/deps.rb
@@ -13,8 +13,9 @@ module Chelsea
       @lockfile = Bundler::LockfileParser.new(File.read(path))
     end
 
-    def nil?
-      @dependencies.empty?
+    def audit
+      lockfile_as_dependencies
+      filter_reverse_dependencies
     end
 
     def self.to_purl(name, version)
@@ -24,43 +25,60 @@ module Chelsea
     # Parses specs from lockfile instanct var and
     # inserts into dependenices instance var
     def dependencies
-      @lockfile.specs.each_with_object({}) do |gem, h|
-        h[gem.name] = [gem.name, gem.version]
-      end
+      @dependencies ||= lockfile_as_dependencies
+      @dependencies
     end
 
-    # Collects all reverse dependencies in reverse_dependencies instance var
-    # this rescue block honks
     def reverse_dependencies
-      _reverse_command
-        .transform_values! do |reverse_dep|
-          reverse_dep.select do |name, _dep, _req, _|
-            _lockfile_specs.include?(name.split('-')[0])
-          end
-        end
+      @reverse_dependencies ||= filter_reverse_dependencies
+      @reverse_dependencies
+    end
+
+    def coordinates
+      @coordinates ||= dependencies_as_coordinates
+      @coordinates
     end
 
     # Iterates over all dependencies and stores them
-    # in dependencies_versions and coordinates instance vars
-    def coordinates
+    # in dependencies_versions giand coordinates instance vars
+    def dependencies_as_coordinates
       dependencies
         .each_with_object({ 'coordinates' => [] }) do |(name, v), coords|
         coords['coordinates'] << self.class.to_purl(name, v[1])
       end
     end
 
-    def _reverse_command
+    # Collects all reverse dependencies in reverse_dependencies instance var
+    # this rescue block honks
+    def filter_reverse_dependencies
+      spec_names = lockfile_specs
+      reverse_command
+        .transform_values! do |reverse_dep|
+          reverse_dep.select do |name, _dep, _req, _|
+            spec_names.include?(name.split('-')[0])
+          end
+        end
+    end
+
+    private
+
+    def lockfile_specs
+      @lockfile.specs.map { |i| i.to_s.split }.map do |n, _v|
+        n.to_s
+      end
+    end
+
+    def reverse_command
       reverse = Gem::Commands::DependencyCommand.new
       reverse.options[:reverse_dependencies] = true
       reverse.options[:pipe_format] = true
       reverse.reverse_dependencies(@lockfile.specs)
     end
 
-    def _lockfile_specs
-      @spec_names ||= @lockfile.specs.map { |i| i.to_s.split }.map do |n, _v|
-        n.to_s
+    def lockfile_as_dependencies
+      @lockfile.specs.each_with_object({}) do |gem, h|
+        h[gem.name] = [gem.name, gem.version]
       end
-      @spec_names
     end
   end
 end
--- lib/chelsea/gems.rb
+++ lib/chelsea/gems.rb
@@ -34,10 +34,11 @@ module Chelsea
 
     # Audits depenencies using deps library and prints results
     # using formatter library
-
+    # Runs through auditing algorithm, raising exceptions
+    # on REST calls made by @deps.get_vulns
     def execute
-      server_response, dependencies, reverse_dependencies = audit
-      if dependencies.nil?
+      server_response = @client.get_vulns(@deps.coordinates)
+      if @deps.dependencies.nil?
         _print_err 'No dependencies retrieved. Exiting.'
         return
       end
@@ -45,60 +46,14 @@ module Chelsea
         _print_err 'No vulnerability data retrieved from server. Exiting.'
         return
       end
-      results = @formatter.get_results(server_response, reverse_dependencies)
+      results = @formatter.get_results(server_response, @deps.reverse_dependencies)
       @formatter.do_print(results)
 
       server_response.map { |r| r['vulnerabilities'].length.positive? }.any?
     end
 
     def collect_iq
-      dependencies = @deps.dependencies
-      dependencies
-    end
-
-    # Runs through auditing algorithm, raising exceptions
-    # on REST calls made by @deps.get_vulns
-    def audit
-      # This spinner management is out of control
-      # we should wrap a block with start and stop messages,
-      # or use a stack to ensure all spinners stop.
-      spin = @spinner.spin_msg 'Parsing dependencies'
-
-      begin
-        dependencies = @deps.dependencies
-        spin.success('...done.')
-      rescue StandardError => e
-        spin.stop
-        _print_err "Parsing dependency line #{gem} failed."
-      end
-
-      reverse_dependencies = @deps.reverse_dependencies
-
-      spin = @spinner.spin_msg 'Parsing Versions'
-      coordinates = @deps.coordinates
-      spin.success('...done.')
-      spin = @spinner.spin_msg 'Making request to OSS Index server'
-
-      begin
-        server_response = @client.get_vulns(coordinates)
-        spin.success('...done.')
-      rescue SocketError => e
-        spin.stop('...request failed.')
-        _print_err 'Socket error getting data from OSS Index server.'
-      rescue RestClient::RequestFailed => e
-        spin.stop('...request failed.')
-        _print_err "Error getting data from OSS Index server:#{e.response}."
-      rescue RestClient::ResourceNotFound => e
-        spin.stop('...request failed.')
-        _print_err 'Error getting data from OSS Index server. Resource not found.'
-      rescue Errno::ECONNREFUSED => e
-        spin.stop('...request failed.')
-        _print_err 'Error getting data from OSS Index server. Connection refused.'
-      rescue StandardError => e
-        spin.stop('...request failed.')
-        _print_err 'UNKNOWN Error getting data from OSS Index server.'
-      end
-      [server_response, dependencies, reverse_dependencies]
+      @deps.dependencies
     end
 
     protected
