02ac99ebbe96dfa413155b4f4f34edeb962e39cf
Jeffry Hesse <5544326+DarthHater@users.noreply.github.com>
2020-06-01 11:48:34 -0800

    Misery

--- .circleci/setup-rubygems.sh
+++ .circleci/setup-rubygems.sh
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
 mkdir ~/.gem
 echo -e "---\r\n:rubygems_api_key: $RUBY_GEMS_API_TOKEN" > ~/.gem/credentials
 chmod 0600 /home/circleci/.gem/credentials
